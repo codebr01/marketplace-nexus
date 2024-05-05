53df0673f0e220b1b1db52be15ce5fe65b95c487
João Neto <joaonetoprivado2001@gmail.com>
2024-05-05 15:17:06 -0300

    chore: login e registro de cliente

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -2,12 +2,13 @@ const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
   if(req.session.user) return res.render('login-logado');
-  return res.render('index');
+  return res.render('index2');
 };
 
 exports.register = async function (req, res) {
   try {
     const login = new Login(req.body);
+
     await login.register();
 
     if (login.errors.length > 0) {
