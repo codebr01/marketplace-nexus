1c079aa906dcdc10ee5c20706ed3f589d2a76d1b
João Neto <joaonetoprivado2001@gmail.com>
2024-07-22 09:33:09 -0300

    fix: correções de bugs no home

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,12 @@
 exports.index = async (req, res) => {
-  if(!req.session.user) {
-    const user = undefined;
-    res.render('index', { user });
+  let user = undefined;
+  if(req.session.user) {
+    if( req.session.loja ) {
+      return res.redirect('/loja/dashboard');
+    }
+    user = req.session.user;
+    return res.render('index', { user });
   }else {
-    const { user } = req.session;
-    res.render('index', { user });
+    return res.render('index', { user });
   }
 };
